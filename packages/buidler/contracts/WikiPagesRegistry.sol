0756ab4c13baaf0451c1c996d414ddab5af3ecef
4MarkusMarkus <4markusmarkus@gmail.com>
2021-08-09 10:53:58 +0800

    code cleaned

--- packages/buidler/contracts/WikiPagesRegistry.sol
+++ packages/buidler/contracts/WikiPagesRegistry.sol
@@ -3,141 +3,115 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 contract WikiPagesRegistry {
-    //   string streamId;
-    //   uint counter = 0;
-    //   struct wikiPage {
-    //       string section1;
-    //       string section2;
-    //       string section3;
-    //       string section4;
-    //       string section5;
-    //       string section6;
-    //       string section7;
-    //       string section8;
-    //       string section9;
-    //       string section10;
+    // registry of wikiPages
+    // TODO create contract => only governance
+    // TODO create contract => only oracle
 
-    //   }
+    // Every section should be always at the same place in the array, this makes it intelligable for the FE.
+    // e.g. FE knows that TVL is always at position 5, so it can ask only for this for all wiki pages
 
-    // registry fo wiki addresses
-    // TODO only governance updates
     // pages => sectionHashes
+    // pages start from 2.nd place => position 1
     string[][] public wikiPages;
 
-    // how many sections does wikipage have
-    // TODO only governance updates
-    mapping(string => uint256) public numOfSections;
+    mapping(string => uint256) public pagePositionInArray;
+    mapping(string => uint256) public lastSectionInArray;
     mapping(string => bool) public pageExists;
-    mapping(string => address) public pageSectionMaintainer;
+    mapping(string => address) public pageSectionMaintainer; //  sectionHash => managerAddres
 
-    function getNumOfPages() public returns (uint256) {
-        return wikiPages.length;
+    constructor() {
+        // this is neccessary for the mappings not to be confusing, they return 0 if no value,
+        // so if array starts from 0 it is confusing
+        wikiPages.push(["Empty page, wikiPages start at position 1"]);
     }
-
-    // TODO only governance
-    function updateNumOfSections() public {}
-
-    // string[] public simplePagesArray;
-
-    // // check if wiki page exists
-    // function pageExists(string memory _wikiPage)
-    //     public
-    //     view
-    //     returns (bool)
-    // {
-    //     if (bytes(wikiPageStreamId[_wikiPage]).length != 0) {
-    //         return true;
-    //     }
-    //     return false;
-    // }
 
     // !! only governance
     function addNewPage(
         string memory _wikiPageName,
-        address _newmaintainer,
-        uint32 _num
+        string[] memory _sectionHashes
     ) external {
         // check if page does not already exists
         require(!pageExists[_wikiPageName], "wiki_page_does_already_exist");
 
         // creates new page in array + writes section hashes
-        simplePagesArray.push(_wikiPage);
+        //// section hashes
+        wikiPages.push(_sectionHashes);
 
-        //
+        // wikiPage position pages start from position 1
+        pagePositionInArray[_wikiPageName] = wikiPages.length - 1;
 
-        //set number of sections
+        //set last section in array
+        lastSectionInArray[_wikiPageName] = _sectionHashes.length - 1;
 
-        // sets maintainer for each section
-        setpageSectionMaintainer(_wikiPage, _newmaintainer);
+        // updates array
+        pageExists[_wikiPageName] = true;
+
+        // // this could be empty if we just create page and allow users to challenge sections
+        // // there are no maintainers needed, anybody can challenge content and become one
+        // // problem could be, that if not set in advance, there could be a fight for becoming one
+        // setpageSectionMaintainer(_wikiPage, _newmaintainer);
 
         // sets page exists
     }
 
-    function createSectionArrayForPage() external returns (string[] storage) {
-        // creates array for new Page
-        hashes[];
-    }
+    // Every section should be always at the same place in the array, this makes it intelligable for the FE.
+    // e.g. FE knows that TVL is always at position 5, so it can ask only for this for all wiki pages
 
-    // if wikPage exists replace array
-    //   // if not add to array
-    //   // create double mappings?
+    // IMPORTANT: push new Sections always to their alloted place in the section array
+    // If the next section is empty, push empty element into array
 
-    //   // change maintainer
-    //   function changemaintainer(string memory _wikiPage, string memory _maintainerId) public returns (bool) {
-    //       pagePosition = wikiPagePosition[_wikiPage];
-    //       // page does not exists
-    //       if(pagePosition == 0) {
-    //           // create new array
-    //       } else {
-    //           // change array at specified position
-    //           wikiRegistry[counter].streamId = _streamId;
-    //           wikiRegistry[pagePosition].wikiPage = _wikiPage;
+    // IMPORTANT: content of each section needs to be unique => it is REQUIRED to put wikiName
 
-    //       }
+    // TODO only governance
+    function addNewSections(
+        string memory _wikiPageName,
+        string[] memory _newSectionHashes
+    ) public {
+        uint256 pagePosition = pagePositionInArray[_wikiPageName];
 
-    //   }
-    //   // if page exists, overwrite array
-    //   // if not, create new array => need mapping that returns position in array.
-
-    // !! change to internal
-    function setpageSectionMaintainer(string memory _wikiPage, address _addr)
-        internal
-    {
-        pageSectionMaintainer[_wikiPage] = _addr;
-    }
-
-    // // !! change to internal
-    // function setWikiPageStreamId(
-    //     string memory _wikiPage,
-    //     string memory _streamId
-    // ) internal {
-    //     // Update the value at this address
-    //     wikiPageStreamId[_wikiPage] = _streamId;
-    // }
-
-    // function setWikiPagePosition(string memory _wikiPage, uint256 _position)
-    //     internal
-    // {
-    //     // Update the value at this address
-    //     wikiPagePosition[_wikiPage] = _position;
-    // }
-
-    // !! add only governance
-    function changemaintainer(address _newmaintainer, string memory _wikiPage)
-        external
-        returns (bool)
-    {
-        // require(
-        //     pageExists(_wikiPage) && _newmaintainer != address(0),
-        //     "wiki_page_does_not_exist"
-        // );
-        setpageSectionMaintainer(_wikiPage, _newmaintainer);
-
-        // checks if maintainer is set to new owner
-        address newmaintainer = pageSectionMaintainer[_wikiPage];
-        if (newmaintainer == _newmaintainer) {
-            return true;
+        for (uint256 i = 0; i < _newSectionHashes.length; i++) {
+            wikiPages[pagePosition].push(_newSectionHashes[i]);
         }
-        return false;
+
+        // wikiPages[pagePosition].push(_newSectionHashes);
+        //
+        // set last section in array
+        lastSectionInArray[_wikiPageName] = wikiPages[pagePosition].length - 1;
+    }
+
+    // ADD Only Oracle
+    function changeMaintainer(
+        uint256 _pagePositionInArray,
+        uint256 _sectionPositionInArray,
+        address _newMaintainer
+    ) public returns (bool) {
+        string memory sectionHash = wikiPages[_pagePositionInArray][
+            _sectionPositionInArray
+        ];
+        pageSectionMaintainer[sectionHash] = _newMaintainer;
+        return true;
+    }
+
+    function updateSection(
+        uint256 _pagePositionInArray,
+        uint256 _sectionPositionInArray,
+        string memory _newSectionHash
+    ) external returns (bool) {
+        // checks if msg.sender == sectionMaintainer
+        require(
+            msg.sender ==
+                pageSectionMaintainer[
+                    wikiPages[_pagePositionInArray][_sectionPositionInArray]
+                ],
+            "you are not maintainer for this section"
+        );
+        wikiPages[_pagePositionInArray][
+            _sectionPositionInArray
+        ] = _newSectionHash;
+        return true;
+    }
+
+    function getNumOfPages() public view returns (uint256) {
+        return wikiPages.length - 1; // comment
     }
 }
