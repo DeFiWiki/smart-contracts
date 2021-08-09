6d79c51479d56c2e15e9ec8a081bbf08559f5725
4MarkusMarkus <4markusmarkus@gmail.com>
2021-08-09 09:24:25 +0800

    second commit

--- packages/buidler/contracts/WikiPagesRegistry.sol
+++ packages/buidler/contracts/WikiPagesRegistry.sol
@@ -6,39 +6,84 @@ contract WikiPagesRegistry {
     //   string streamId;
     //   uint counter = 0;
     //   struct wikiPage {
-    //       string streamId;
-    //       string wikiPage;
+    //       string section1;
+    //       string section2;
+    //       string section3;
+    //       string section4;
+    //       string section5;
+    //       string section6;
+    //       string section7;
+    //       string section8;
+    //       string section9;
+    //       string section10;
+
     //   }
 
     // registry fo wiki addresses
-    // wikiPage[100] public wikiPages;
+    // TODO only governance updates
+    // pages => sectionHashes
+    string[][] public wikiPages;
 
-    string[] public simplePagesArray;
+    // how many sections does wikipage have
+    // TODO only governance updates
+    mapping(string => uint256) public numOfSections;
+    mapping(string => bool) public pageExists;
+    mapping(string => address) public pageSectionMaintainer;
 
-    mapping(string => address) public wikiPageController;
+    function getNumOfPages() public returns (uint256) {
+        return wikiPages.length;
+    }
 
-    mapping(string => string) public wikiPageStreamId;
+    // TODO only governance
+    function updateNumOfSections() public {}
 
-    mapping(string => uint256) public wikiPagePosition;
+    // string[] public simplePagesArray;
 
-    // check if wiki page exists
-    function wikiPageExists(string memory _wikiPage)
-        public
-        view
-        returns (bool)
-    {
-        if (bytes(wikiPageStreamId[_wikiPage]).length != 0) {
-            return true;
-        }
-        return false;
+    // // check if wiki page exists
+    // function pageExists(string memory _wikiPage)
+    //     public
+    //     view
+    //     returns (bool)
+    // {
+    //     if (bytes(wikiPageStreamId[_wikiPage]).length != 0) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    // !! only governance
+    function addNewPage(
+        string memory _wikiPageName,
+        address _newmaintainer,
+        uint32 _num
+    ) external {
+        // check if page does not already exists
+        require(!pageExists[_wikiPageName], "wiki_page_does_already_exist");
+
+        // creates new page in array + writes section hashes
+        simplePagesArray.push(_wikiPage);
+
+        //
+
+        //set number of sections
+
+        // sets maintainer for each section
+        setpageSectionMaintainer(_wikiPage, _newmaintainer);
+
+        // sets page exists
+    }
+
+    function createSectionArrayForPage() external returns (string[] storage) {
+        // creates array for new Page
+        hashes[];
     }
 
     // if wikPage exists replace array
     //   // if not add to array
     //   // create double mappings?
 
-    //   // change controller
-    //   function changeController(string memory _wikiPage, string memory _controllerId) public returns (bool) {
+    //   // change maintainer
+    //   function changemaintainer(string memory _wikiPage, string memory _maintainerId) public returns (bool) {
     //       pagePosition = wikiPagePosition[_wikiPage];
     //       // page does not exists
     //       if(pagePosition == 0) {
@@ -55,65 +100,42 @@ contract WikiPagesRegistry {
     //   // if not, create new array => need mapping that returns position in array.
 
     // !! change to internal
-    function setwikiPageController(string memory _wikiPage, address _addr)
+    function setpageSectionMaintainer(string memory _wikiPage, address _addr)
         internal
     {
-        wikiPageController[_wikiPage] = _addr;
+        pageSectionMaintainer[_wikiPage] = _addr;
     }
 
-    // !! change to internal
-    function setWikiPageStreamId(
-        string memory _wikiPage,
-        string memory _streamId
-    ) internal {
-        // Update the value at this address
-        wikiPageStreamId[_wikiPage] = _streamId;
-    }
+    // // !! change to internal
+    // function setWikiPageStreamId(
+    //     string memory _wikiPage,
+    //     string memory _streamId
+    // ) internal {
+    //     // Update the value at this address
+    //     wikiPageStreamId[_wikiPage] = _streamId;
+    // }
 
-    function setWikiPagePosition(string memory _wikiPage, uint256 _position)
-        internal
-    {
-        // Update the value at this address
-        wikiPagePosition[_wikiPage] = _position;
-    }
+    // function setWikiPagePosition(string memory _wikiPage, uint256 _position)
+    //     internal
+    // {
+    //     // Update the value at this address
+    //     wikiPagePosition[_wikiPage] = _position;
+    // }
 
-    // !! only governance
-    function addNewPage(
-        string memory _wikiPage,
-        address _newController,
-        string memory _streamId
-    ) external {
-        // check if page does not already exists
-        require(!wikiPageExists(_wikiPage), "wiki_page_does_already_exist");
-
-        // creates new element in array
-        simplePagesArray.push(_wikiPage);
-
-        //set wiki page position
-        uint256 _position = simplePagesArray.length;
-        setWikiPagePosition(_wikiPage, _position);
-
-        // sets controller
-        setwikiPageController(_wikiPage, _newController);
-
-        // sets stream
-        setWikiPageStreamId(_wikiPage, _streamId);
-    }
-
-    // !! add only owner
-    function changeController(address _newController, string memory _wikiPage)
+    // !! add only governance
+    function changemaintainer(address _newmaintainer, string memory _wikiPage)
         external
         returns (bool)
     {
-        require(
-            wikiPageExists(_wikiPage) && _newController != address(0),
-            "wiki_page_does_not_exist"
-        );
-        setwikiPageController(_wikiPage, _newController);
+        // require(
+        //     pageExists(_wikiPage) && _newmaintainer != address(0),
+        //     "wiki_page_does_not_exist"
+        // );
+        setpageSectionMaintainer(_wikiPage, _newmaintainer);
 
-        // checks if controller is set to new owner
-        address newController = wikiPageController[_wikiPage];
-        if (newController == _newController) {
+        // checks if maintainer is set to new owner
+        address newmaintainer = pageSectionMaintainer[_wikiPage];
+        if (newmaintainer == _newmaintainer) {
             return true;
         }
         return false;
